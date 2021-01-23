55e6c7611b50d5a2ac475ec77049df3260c1df0f
Philip Laxamana <philip.laxamana@hotmail.com>
2021-01-23 14:40:40 -0500

    adding collision logic

--- module-4/snake-game/script.js
+++ module-4/snake-game/script.js
@@ -4,6 +4,7 @@ const score = document.getElementById('score')
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
+let width = 10
 
 const createGrid = () => {
     // create 100 of these elements
@@ -26,6 +27,15 @@ createGrid();
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 const move = () => {
+    if (
+        (currentSnake[0] + width >= 100 && direction === 10) || // if snake has hit bottom
+        (currentSnake[0] % 10 === 9) || // if snake has hit right wall
+        (currentSnake[0] % 10 === 0) || // if snake has hit left wall
+        (currentSnake[0] - width < 0 && direction === -10)// if snake has hit top 
+    )
+    return clearInterval(timerId)
+
+
     // remove last element from our currentSnake array
     const tail = currentSnake.pop()
 
@@ -42,7 +52,7 @@ const move = () => {
 
 move();
 
-// let timerId = setInterval(move, 1000)
+let timerId = setInterval(move, 500)
 
 // 39 is right arrow
 // 38 is for up arrow
@@ -53,11 +63,17 @@ const control = (e) => {
     // if right arrow is pressed
     if (e.keyCode === 39) {
         console.log('right pressed')
+        direction = 1
     } else if (e.keyCode === 38) {
         console.log('up pressed')
+        direction = -width
     } else if (e.keyCode === 37) {
         console.log('left pressed')
+        direction = -1
     } else if (e.keyCode === 40){
         console.log('down pressed')
+        direction = +width
     }
 }
+
+document.addEventListener('keydown', control)

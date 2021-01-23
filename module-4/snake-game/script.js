339da1324efe30f6323c765f6b1fff8c8c70c1e8
Philip Laxamana <philip.laxamana@hotmail.com>
2021-01-23 11:51:36 -0500

    movement and styling added to snake

--- module-4/snake-game/script.js
+++ module-4/snake-game/script.js
@@ -2,6 +2,8 @@ const grid = document.querySelector('.grid')
 const startBtn = document.getElementById('start')
 const score = document.getElementById('score')
 let squares = []
+let currentSnake = [2,1,0]
+let direction = 1
 
 const createGrid = () => {
     // create 100 of these elements
@@ -15,7 +17,47 @@ const createGrid = () => {
         // push it into a new quares array
         squares.push(square)
     }
-    console.log(squares)
+    // console.log(squares)
 }
 
+
 createGrid();
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
+
+const move = () => {
+    // remove last element from our currentSnake array
+    const tail = currentSnake.pop()
+
+    // remove the styling from the last element
+    squares[tail].classList.remove('snake')
+
+    // add a square in direction we are heading
+    currentSnake.unshift(currentSnake[0] + direction)
+    // const head = currentSnake.unshift(currentSnake[0] + direction)
+
+    // add styling to see it
+    squares[currentSnake[0]].classList.add('snake')
+}
+
+move();
+
+// let timerId = setInterval(move, 1000)
+
+// 39 is right arrow
+// 38 is for up arrow
+// 37 is for left arrow
+// 40 is for the down arrow
+
+const control = (e) => {
+    // if right arrow is pressed
+    if (e.keyCode === 39) {
+        console.log('right pressed')
+    } else if (e.keyCode === 38) {
+        console.log('up pressed')
+    } else if (e.keyCode === 37) {
+        console.log('left pressed')
+    } else if (e.keyCode === 40){
+        console.log('down pressed')
+    }
+}

680a75c6a9b5e07fedc8f0ccd01989fa00104dda
Philip Laxamana <philip.laxamana@hotmail.com>
2021-01-30 21:36:25 -0500

    added pagination

--- module-7/beer-punk/index.js
+++ module-7/beer-punk/index.js
@@ -1,13 +1,78 @@
 // variables
-const urlBase = "https://api.punkapi.com/v2/beers";
+const urlBase = "https://api.punkapi.com/v2/beers?page="
+const filterABV = document.getElementById('filterABV')
+const filterIBU = document.getElementById('filterIBU')
+const pageText = document.getElementById('pageNumber')
+const prevPage = document.getElementById('prevPage')
+const nextPage = document.getElementById('nextPage')
 const beersDiv = document.querySelector('.beers')
 
+let optionsABV = '', optionsIBU = '', page = 1
+
+filterABV.addEventListener('change', (e) => {
+    const value = e.target.value
+
+    switch(value) {
+        case 'all':
+            optionsABV = '';
+            break
+        case 'weak':
+            optionsABV = '&abv_lt=4.6'
+            break
+        case 'medium':
+            optionsABV = '&abv_gt=4.5&abv_lt=7.6'
+            break
+        case 'strong':
+            optionsABV = '&abv_gt=7.5'
+            break
+    }
+    page = 1
+    getBeers()
+})
+
+filterIBU.addEventListener('change', (e) => {
+    const value = e.target.value 
+
+    switch(value) {
+        case 'all':
+            optionsIBU = '';
+            break
+        case 'weak':
+            optionsIBU = '&ibu_lt=35'
+            break
+        case 'medium':
+            optionsIBU = '&ibu_gt=34&ibu_lt=75'
+            break
+        case 'strong':
+            optionsIBU = '&ibu_gt=74'
+            break 
+    }
+    page = 1
+    getBeers()
+})
+
+
 const getBeers = async () => {
-    const res = await fetch(urlBase)
+    const url = urlBase + page + optionsABV + optionsIBU
+    const res = await fetch(url)
+    console.log(url)
     let beerHtml = ''
+    
     try {
         const beers = await res.json()
-        console.log(beers[0])
+        pageText.innerText = page;
+        if(page === 1) {
+            prevPage.disabled = true
+        } else {
+            prevPage.disabled = false
+        }
+
+        if (beers.length < 25) {
+            nextPage.disabled = true
+        } else {
+            nextPage.disabled = false
+        }
+
         for(let beer of beers) {
             beerHtml += `
             <div class="beer-wrapper card">
@@ -36,5 +101,16 @@ const getBeers = async () => {
         console.log(e)
     }
 }
+
+// pagination
+prevPage.addEventListener('click', () => {
+    page--
+    getBeers()
+})
+
+nextPage.addEventListener('click', () => {
+    page++
+    getBeers()
+})
 
 getBeers()

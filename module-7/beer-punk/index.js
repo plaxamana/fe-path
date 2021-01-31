a64e54294f0423a03057909d93a0878f8795be24
Philip Laxamana <philip.laxamana@hotmail.com>
2021-01-30 20:52:09 -0500

    getting our beers displayed from api

---
+++ module-7/beer-punk/index.js
@@ -0,0 +1,32 @@
+// variables
+const urlBase = "https://api.punkapi.com/v2/beers";
+const beersDiv = document.querySelector('.beers')
+
+const getBeers = async () => {
+    const res = await fetch(urlBase)
+    let beerHtml = ''
+    try {
+        const beers = await res.json()
+        console.log(beers[0])
+        for(let beer of beers) {
+            beerHtml += `
+            <div class="beer-wrapper card">
+                <div class="beer">
+                    <img class='beer__img' src='${beer.image_url}' />
+                    <h3>${beer.name}</h3>
+                    <span class='beer__info'>
+                        <span>ABV: ${beer.abv}%</span>
+                        <span>IBU: ${beer.ibu}</span>
+                    </span>
+                </div>
+            </div>
+            `
+        }
+        
+        beersDiv.innerHTML = beerHtml
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+getBeers()
